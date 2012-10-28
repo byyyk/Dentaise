4c933f90e24cfb38ec80547495c152ad0d6ffd02
Mateusz Kulpa <byku88@gmail.com>
2012-10-28 20:23:13 +0100

    Added dentaise-server

---
+++ dentaise-server/src/main/java/pl/edu/agh/mkulpa/dentaise/server/DentaiseRestWebService.java
@@ -0,0 +1,15 @@
+package pl.edu.agh.mkulpa.dentaise.server;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/notes")
+@Produces({"application/xml"})
+public class DentaiseRestWebService {
+    @GET
+    public String list() {
+        return "<b>Hello World, I still need some work to be useful!</b>";
+    }
+
+}
