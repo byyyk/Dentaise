7c2758d93de90c38f92833304872940eb163ba62
Mateusz Kulpa <m.c.kulpa@gmail.com>
2014-07-15 23:24:48 +0200

    Revert "migrated to play 2.3.1"
    This reverts commit 7d6617ada5046a79d7a5e251f003ee7128b1102a.

--- dentaise-web/project/Build.scala
+++ dentaise-web/project/Build.scala
@@ -1,6 +1,6 @@
 import sbt._
-import play.Play.autoImport._
-import PlayKeys._
+import Keys._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
@@ -16,7 +16,7 @@ object ApplicationBuild extends Build {
     "org.hibernate" % "hibernate-entitymanager" % "4.1.9.Final"
   )
 
-  val main = Project(appName, file(".")).enablePlugins(play.PlayJava).settings(
+  val main = play.Project(appName, appVersion, appDependencies).settings(
   	ebeanEnabled := false 
     // Add your own project settings here      
   )
